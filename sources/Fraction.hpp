d361bb1bd69df3dcedaa6cd00efd7c9e21dd3897
ubuntu <itamar.kraitman@msmail.ariel.ac.il>
2023-04-16 10:41:01 +0200

    fix

---
+++ sources/Fraction.hpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+namespace ariel
+{
+    class Fraction
+    {
+    private:
+        int nominator;
+        int denominator;
+
+    public:
+        Fraction(int nominator, int denominator);
+        int getNominator() const;
+        int geDenominator() const;
+
+        // overloading +,-,*,/,==,>,<,<=,>=,++,--,<<,>>
+        Fraction operator+(const Fraction &other);
+        Fraction operator-(const Fraction &other);
+        Fraction operator*(const Fraction &other);
+        Fraction operator/(const Fraction &other);
+
+        Fraction operator*(double floatNumber);
+        Fraction operator+(double floatNumber);
+        Fraction operator-(double floatNumber);
+        Fraction operator/(double floatNumber);
+
+        Fraction operator++(int dummy_flag);
+        Fraction operator--(int dummy_flag);
+        Fraction &operator++();
+        Fraction &operator--();
+        // std::ostream& operator<<(std::ostream& os, const Fraction& f);
+
+
+        friend bool operator+( double floatNumber, const Fraction &f1);
+        friend bool operator-( double floatNumber, const Fraction &f1);
+        friend bool operator*( double floatNumber, const Fraction &f1);
+        friend bool operator/( double floatNumber, const Fraction &f1);
+
+        
+
+        friend bool operator==(const Fraction &f1, const Fraction &f2);
+        friend bool operator>=(const Fraction &f1, const Fraction &f2);
+        friend bool operator<=(const Fraction &f1, const Fraction &f2);
+        friend bool operator>(const Fraction &f1, const Fraction &f2);
+        friend bool operator<(const Fraction &f1, const Fraction &f2);
+        friend bool operator==(const Fraction &f1, double floatNumber);
+        friend bool operator>=(const Fraction &f1, double floatNumber);
+        friend bool operator<=(const Fraction &f1, double floatNumber);
+        friend bool operator>(const Fraction &f1, double floatNumber);
+        friend bool operator<(const Fraction &f1, double floatNumber);
+
+        friend std::ostream &operator<<(std::ostream &output, const Fraction &f);
+        friend std::istream &operator>>(std::istream &input, Fraction &f);
+    };
+}
