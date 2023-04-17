17d2ba25a1027aebb7086e21e3f84c9ea188e903
itamarKraitman <itamar.k96@gmail.com>
2023-04-17 17:07:23 +0300

    finish testing

--- Test.cpp
+++ Test.cpp
@@ -4,6 +4,8 @@
 #include <random>
 #include <cstdlib>
 #include <ctime>
+#include <sstream>
+#include <fstream>
 #include "doctest.h"
 #include "sources/Fraction.hpp"
 
@@ -19,8 +21,6 @@ TEST_CASE("Zero at the denominator- deviding by zero")
         CHECK_THROWS(Fraction(num, 0));
     }
 }
-
-
 
 TEST_CASE("Methods do not throw errors- fraction with fraction")
 {
@@ -89,7 +89,7 @@ TEST_CASE("Logic operators work as expected- fraction and fraction")
 
 TEST_CASE("Logic operatos works as expectd- float number and fraction")
 {
-    Fraction a(1,2);
+    Fraction a(1, 2);
     CHECK(a > 0.1);
     CHECK(0.111 < a);
     CHECK(a == 0.5);
@@ -97,12 +97,12 @@ TEST_CASE("Logic operatos works as expectd- float number and fraction")
 
 TEST_CASE("Negative numerator and denominator")
 {
-    Fraction a(-5,-3); // -5/-3 == 5/3
+    Fraction a(-5, -3); // -5/-3 == 5/3
     Fraction b(5, 3);
     CHECK(a == b);
-    Fraction c(-5,3); //-5/3
+    Fraction c(-5, 3); //-5/3
     CHECK(a > c);
-    Fraction d(5,-3); // 5/-3 == -5/3 
+    Fraction d(5, -3); // 5/-3 == -5/3
     CHECK(c == d);
     CHECK(d < b);
 }
@@ -112,8 +112,43 @@ TEST_CASE("reduced and unreduced fractions are equal")
     for (int i = 1; i < 1000; i++)
     {
         Fraction a(i, i + 1);
-        Fraction b(i * 3, (i + 1 ) * 3); 
+        Fraction b(i * 3, (i + 1) * 3);
         CHECK(a == b); // 'a' and 'b' should be rqual! eg 1/2 == 2/4 == 3/6
     }
-    
+}
+
+TEST_CASE("Binary operators works as excpected")
+{
+    Fraction a(1, 2);
+    Fraction b(1, 4);
+    Fraction c = a + b;
+    CHECK(c == Fraction(3, 4));
+    c = a - b;
+    CHECK(c == Fraction(1, 4));
+    c = a / b;
+    CHECK(c == Fraction(2, 1));
+    c = a * b;
+    CHECK(c == Fraction(1, 8));
+    c = 1.1 * a;
+    CHECK(c == Fraction(8, 5));
+    c = b + 2.421;
+    CHECK(c == Fraction(2671, 1000));
+    c = a + b - 1;
+    CHECK(c == Fraction(-1, 4));
+}
+
+TEST_CASE("<< operator works correctly")
+{
+     std::ostringstream output;
+     Fraction a(1,2);
+     output << a;
+     CHECK(output.str() == "1/2");
+     output.clear();
+     Fraction b(1,-2);
+     output << b;
+     CHECK(output.str() == "1/-2");
+     output.clear();
+     Fraction c(-1,2);
+     output << c;
+     CHECK(output.str() == "-1/2");
 }
